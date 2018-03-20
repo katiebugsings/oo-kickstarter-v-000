a8eb621a4985e6e8aeb51194c568bfcb5b3ff37e
Katie Cassell <kcassell944@gmail.com>
2018-03-20 17:02:50 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,5 +1,5 @@
 class Backer
-  attr_accessor :backed_projects, :name, :backer
+  attr_accessor :backed_projects, :name
 
   def initialize(name)
     @name = name
@@ -8,6 +8,5 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    @backers << backer
   end
 end
